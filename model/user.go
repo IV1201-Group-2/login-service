308aa776ff6673d344b84330bca28d93c3c30e4f
Hannes Mann <hannesmann2000@gmail.com>
2024-02-02 14:08:25 +0100

    Add user model and JWT claims

---
+++ model/user.go
@@ -0,0 +1,28 @@
+package model
+
+import "github.com/golang-jwt/jwt/v5"
+
+// TODO: make sure this syncs up with the database
+
+type Role int
+
+const (
+	RoleApplicant Role = iota
+	RoleRecruiter
+)
+
+type User struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+
+	Name           string `json:"name"`
+	Surname        string `json:"surname"`
+	PersonalNumber string `json:"pnr"`
+
+	Role Role `json:"role"`
+}
+
+type UserClaims struct {
+	User
+	jwt.RegisteredClaims
+}
